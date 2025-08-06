76ba8f2682d929e71455d69f866d8d13e8e18267
FranMeza <ing.meza.francisco@gmail.com>
2025-08-06 09:18:14 -0600

    implement score submission and result redirection logic

--- app1.py
+++ app1.py
@@ -8,5 +8,34 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/fail/<int:score>")
+def fail(score):
+    return f"You have failed with a score of {score}"
+
+
+@app.route("/pass/<int:score>")
+def pass_score(score):
+    return f"You have successfully pass with a score of {score}"
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    total_score = 0
+    if request.method == "POST":
+        DATA440 = float(request.form["data440"])
+        ARTI404 = float(request.form["arti404"])
+        ARTI406 = float(request.form["arti406"])
+        ARTI408 = float(request.form["arti408"])
+
+        total_score = (DATA440 + ARTI404 + ARTI406 + ARTI408) / 4
+
+    if total_score < 50:
+        res = redirect(url_for("fail", score=total_score))
+
+    else:
+        res = redirect(url_for("pass_score", score=total_score))
+    return res
+
+
 if __name__ == "__main__":
     app.run(debug=True)
